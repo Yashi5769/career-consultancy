ccf35d40918da60a9ee116bfec43c80199abb2c1
Yashi . <yashi.107606@stu.upes.ac.in>
2025-07-17 10:04:26 +0530

    Delete career_consulting directory

--- career_consulting/career/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ResumeForm(forms.Form):
-    resume = forms.FileField(label="Upload Resume (PDF)", required=True)
-
-class FeedbackForm(forms.Form):
-    feedback = forms.CharField(widget=forms.Textarea, label="Your Feedback")
