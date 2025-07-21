6ae07f523d8e012917fc091c118c58cc8cd45ddf
Yashi . <yashi.107606@stu.upes.ac.in>
2025-07-21 11:39:20 +0530

    Delete career directory

--- career/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CareerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    # --- FIX: This name MUST match the full path to your app ---
-    name = 'career_consulting.career'
-
